c39b7fc7adf93f170a48c4143e266aacded0d692
William Zujkowski <williamzujkowski@gmail.com>
2024-12-31 13:31:12 -0500

    normalize.css and other layout tweaks

--- main.js
+++ main.js
@@ -46,8 +46,7 @@
           <a href="https://github.com/williamzujkowski" target="_blank">GitHub</a> |
           <a href="https://www.linkedin.com/in/williamzujkowski/" target="_blank">LinkedIn</a> |
           <a href="https://steamcommunity.com/id/grenlan/" target="_blank">Steam</a>
-          <!-- Hidden message -->
-          <span id="secret-message">You found the secret! 🕵️‍♂️</span>
+
         </small>
       `;
         return footer;
