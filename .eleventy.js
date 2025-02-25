89b8fd5b26a686092a42f61f972de385bbcf88fc
William Zujkowski <williamzujkowski@gmail.com>
2025-02-24 23:58:55 -0500

    Test signed commit

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,25 @@
+const yaml = require("js-yaml");
+const { DateTime } = require("luxon");
+
 module.exports = function(eleventyConfig) {
+  // Add support for YAML data files
+  eleventyConfig.addDataExtension("yml", contents => yaml.load(contents));
+  
   // Copy assets directory to the output
   eleventyConfig.addPassthroughCopy("src/assets");
+  
+  // Date formatting filter
+  eleventyConfig.addFilter("formatDate", (dateObj, format = "LLL d, yyyy") => {
+    return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat(format);
+  });
+  
+  // Current year shortcode
+  eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
+  
+  // Limit filter for collections
+  eleventyConfig.addFilter("limit", (array, limit) => {
+    return array.slice(0, limit);
+  });
   
   // Set custom directories for input, output, includes, and data
   return {
