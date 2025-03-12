7671fe9fb11052416e30c6f831c9096c6f199b51
William Zujkowski <williamzujkowski@gmail.com>
2025-03-12 01:42:58 -0400

    major overhaul

--- assets/js/footer_gen.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    fetch("/includes/footer.html")
-        .then(response => response.text())
-        .then(html => {
-            document.getElementById("dynamic-footer").innerHTML = html;
-        })
-        .catch(err => console.error("Error loading footer:", err));
-});
